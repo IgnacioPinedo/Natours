475303f922379cc0abda087eb840f1d59c5e6efb
IgnacioPinedo <ignacio.pinedo@outlook.com>
2020-06-08 12:48:11 -0300

    Fixed updatePassword bug

--- controllers/authController.js
+++ controllers/authController.js
@@ -180,7 +180,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
 });
 
 exports.updatePassword = catchAsync(async (req, res, next) => {
-  const user = await (await User.findById(req.user.id)).select('+password');
+  const user = await User.findById(req.user.id).select('+password');
 
   if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
     return next(new AppError('Your current password is wrong.', 401));
